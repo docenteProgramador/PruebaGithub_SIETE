5b0407e61475a32fc381b801539beec675452270
dbueno008 <danielbueno@unitec.edu>
2026-08-24 20:42:46 -0600

    Mostrar en pantalla usando los prints y captura de datos al declarar variables

--- EjercicioGitHub/src/ejerciciogithub/EjercicioGitHub.java
+++ EjercicioGitHub/src/ejerciciogithub/EjercicioGitHub.java
@@ -4,6 +4,8 @@
  */
 package ejerciciogithub;
 
+import java.util.Scanner;
+
 /**
  *
  * @author oscar
@@ -17,6 +19,8 @@ public class EjercicioGitHub {
         // TODO code application logic here
         //Este es el cascaron de GITHUB/....Oscar Andrade estuvo Aqui!!!
         //Probando de Nuevo
-    }
+         Scanner sc = new Scanner(System.in);
+        
+    }// fin Main
     
-}
+}// Fin Class
