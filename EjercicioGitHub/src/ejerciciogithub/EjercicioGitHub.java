d15857c56c194e7241efe7ea8eea54cde923c8a8
Lianahernandez1004 <liana_hernandez@unitec.edu>
2026-08-25 04:14:02 +0200

    EjercicioGitHub

--- EjercicioGitHub/src/ejerciciogithub/EjercicioGitHub.java
+++ EjercicioGitHub/src/ejerciciogithub/EjercicioGitHub.java
@@ -4,6 +4,8 @@
  */
 package ejerciciogithub;
 
+import java.util.Scanner;
+
 /**
  *
  * @author oscar
@@ -17,6 +19,8 @@ public class EjercicioGitHub {
         // TODO code application logic here
         //Este es el cascaron de GITHUB/....Oscar Andrade estuvo Aqui!!!
         //Probando de Nuevo
-    }
+        Scanner input = new Scanner(System.in);
+        
+    }// Fin de MAin
     
-}
+}// Fin de Class
